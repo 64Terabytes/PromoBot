b3914e8a497cf9b01f532a7d1e771a3c9f1cc625
64Terabytes <68991396+64Terabytes@users.noreply.github.com>
2022-03-13 11:43:37 +0000

    Fuck Python, JS is my new best friend

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js')
+const client = new Discord.Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES"
+  ]
+})
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong")
+  }
+})
+
+client.on("messageCreate", (message) => {
+  if (message.content == "hi"){
+    message.reply("Hello World")
+  }
+})
+
+client.login(process.env['DiscordBotToken'])
